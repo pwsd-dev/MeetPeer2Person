e884e47384f042a35fe70433c66f68fc65e19290
Nikita Shvetsov <nshvtsv@gmail.com>
2022-05-12 09:50:39 -0700

    add

---
+++ src/storeMobx/items.js
@@ -0,0 +1,25 @@
+import { observable, computed, action } from "mobx";
+
+class Items {
+  @observable items = getItems();
+}
+
+function getItems() {
+  return [
+    {
+      id: 100,
+      title: "Iphone x",
+      price: 58999,
+      rest: 54,
+      current: 1,
+    },
+
+    {
+      id: 101,
+      title: "Iphone 8",
+      price: 39900,
+      rest: 15,
+      current: 1,
+    },
+  ];
+}
